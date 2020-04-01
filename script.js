62feb6d3ae7e56ffadafaca5413b6a890e2667fb
Iselk <62830417+Iselk@users.noreply.github.com>
2020-04-01 20:28:32 +0200

    Added hue support

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ document.getElementById('themeButton').addEventListener('click', changeTheme);
 document.getElementById('hueButton').addEventListener('click', changeHue);
 
 const themes = ['dark', 'light'];
-const hues = ['normal', 'spolar'];
+const hues = ['normal', 'shift'];
 
 function changeTheme ()
 {
-	console.log('changing theme');
 	if(localStorage.getItem('theme') == themes[0])
 	{
 		localStorage.setItem('theme', themes[1]);
@@ -18,17 +17,37 @@ function changeTheme ()
 		document.body.classList.replace(themes[1], themes[0]);
 	}
 }
+
 function changeHue ()
 {
-	console.log('changing hur');
+	if(localStorage.getItem('hue') == hues[0])
+	{
+		localStorage.setItem('hue', hues[1]);
+		document.body.classList.replace(hues[0], hues[1]);
 
+	} else
+	{
+		localStorage.setItem('hue', hues[0]);
+		document.body.classList.replace(hues[1], hues[0]);
+	}
 }
 
+// Load or define theme setting in localStorage
 if(localStorage.getItem('theme'))
 {
 	document.body.classList.add(localStorage.getItem('theme'));
 } else
 {
-	localStorage.setItem('theme', 'dark');
+	localStorage.setItem('theme', themes[0]);
 	document.body.classList.add(localStorage.getItem('theme'));
+}
+
+// Load or define hue setting in localStorage
+if(localStorage.getItem('hue'))
+{
+	document.body.classList.add(localStorage.getItem('hue'));
+} else
+{
+	localStorage.setItem('hue', hues[0]);
+	document.body.classList.add(localStorage.getItem('hue'));
 }
