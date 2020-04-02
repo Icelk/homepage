6edf75bd806edec20bd9386c987c9af43ca4a812
Iselk <62830417+Iselk@users.noreply.github.com>
2020-04-02 18:59:47 +0200

    Added theme and hue buttons text&fixed transitions

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById('themeButton').addEventListener('click', changeTheme);
 document.getElementById('hueButton').addEventListener('click', changeHue);
 
 const themes = ['dark', 'light'];
-const hues = ['normal', 'shift'];
+const hues = ['normal', 'shift', 'polar', 'solar'];
 
 function changeTheme ()
 {
@@ -10,11 +10,20 @@ function changeTheme ()
 	{
 		localStorage.setItem('theme', themes[1]);
 		document.body.classList.replace(themes[0], themes[1]);
-
+		document.getElementById('themeText').innerHTML = themes[0];
+		if(document.getElementById('hueText').innerHTML != hues[0])
+		{
+			document.getElementById('hueText').innerHTML = hues[3];
+		}
 	} else
 	{
 		localStorage.setItem('theme', themes[0]);
 		document.body.classList.replace(themes[1], themes[0]);
+		document.getElementById('themeText').innerHTML = themes[1];
+		if(document.getElementById('hueText').innerHTML != hues[0])
+		{
+			document.getElementById('hueText').innerHTML = hues[2];
+		}
 	}
 }
 
@@ -24,11 +33,18 @@ function changeHue ()
 	{
 		localStorage.setItem('hue', hues[1]);
 		document.body.classList.replace(hues[0], hues[1]);
-
+		document.getElementById('hueText').innerHTML = hues[0];
 	} else
 	{
 		localStorage.setItem('hue', hues[0]);
 		document.body.classList.replace(hues[1], hues[0]);
+		if(localStorage.getItem('theme') == themes[0])
+		{
+			document.getElementById('hueText').innerHTML = hues[2];
+		} else
+		{
+			document.getElementById('hueText').innerHTML = hues[3];
+		}
 	}
 }
 
@@ -36,18 +52,30 @@ function changeHue ()
 if(localStorage.getItem('theme'))
 {
 	document.body.classList.add(localStorage.getItem('theme'));
+	document.getElementById('themeText').innerHTML = (localStorage.getItem('theme') == themes[0]) ? themes[1] : themes[0];
 } else
 {
 	localStorage.setItem('theme', themes[0]);
 	document.body.classList.add(localStorage.getItem('theme'));
+	document.getElementById('themeText').innerHTML = (localStorage.getItem('theme') == themes[0]) ? themes[1] : themes[0];
 }
 
 // Load or define hue setting in localStorage
 if(localStorage.getItem('hue'))
 {
 	document.body.classList.add(localStorage.getItem('hue'));
+	document.getElementById('hueText').innerHTML = (localStorage.getItem('theme') == themes[0]) ? hues[2] : hues[3];
 } else
 {
 	localStorage.setItem('hue', hues[0]);
 	document.body.classList.add(localStorage.getItem('hue'));
+	document.getElementById('hueText').innerHTML = (localStorage.getItem('theme') == themes[0]) ? hues[2] : hues[3];
+}
+
+window.onload = () =>
+{
+	setTimeout(() =>
+	{
+		document.body.classList.remove('loading');
+	}, 0);
 }
